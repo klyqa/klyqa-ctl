37dc7456ba2547798350e71dcf2bdfb26e3ff5cc
Frederick Stallmeyer <frederick.stallmeyer@qconnex.com>
2022-09-16 01:04:40 +0200

    Add init package file

---
+++ __init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+###################################################################
+#
+#
+# Interactive Klyqa Control commandline client package
+#
+#
+# Company: QConnex GmbH / Klyqa
+# Author: Frederick Stallmeyer
+#
+# E-Mail: frederick.stallmeyer@gmx.de
+#
+#
+###################################################################
+
+from .klyqa_ctl import Klyqa_account
+
