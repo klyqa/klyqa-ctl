13612e77a8c39aecf57a303045181fb688aac34d
Frederick Stallmeyer <fraizy@gmx.de>
2022-09-16 11:36:43 +0200

    Bump version to 1.0.2

--- setup.py
+++ setup.py
@@ -2,9 +2,8 @@ import setuptools
 
 setuptools.setup(
     name="klyqa_ctl",
-    version="1.0.1",
+    version="1.0.2",
     author="Frederick Stallmeyer",
     description="Control the Klyqa Smart Devices.",
     packages=["klyqa_ctl"],
-    
 )
