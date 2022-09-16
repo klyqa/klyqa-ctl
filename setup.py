b3f433de5b8ccd15262311bec8a573d0e9f7ff96
Frederick Stallmeyer <fraizy@gmx.de>
2022-09-16 14:37:27 +0200

    New version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="klyqa_ctl",
-    version="1.0.2",
+    version="1.0.3",
     author="Frederick Stallmeyer",
     description="Control the Klyqa Smart Devices.",
     packages=["klyqa_ctl"],
