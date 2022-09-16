d0a727dbc39d8d76fa945980058b1b412b0863f1
Frederick Stallmeyer <fraizy@gmx.de>
2022-09-16 11:15:26 +0200

    Make pypi repository, adjust readme

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="klyqa_ctl",
+    version="1.0.1",
+    author="Frederick Stallmeyer",
+    description="Control the Klyqa Smart Devices.",
+    packages=["klyqa_ctl"],
+    
+)
