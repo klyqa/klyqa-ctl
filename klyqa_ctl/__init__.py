d9fe8e2d928ae518131e5e510b0e6013318509ed
Frederick Stallmeyer <fraizy@gmx.de>
2022-10-17 10:02:49 +0200

    Better pypi package management. Giving a commandline starter klyqa-ctl.

--- klyqa_ctl/__init__.py
+++ klyqa_ctl/__init__.py
@@ -2,17 +2,18 @@
 
 ###################################################################
 #
-#
 # Interactive Klyqa Control commandline client package
-#
 #
 # Company: QConnex GmbH / Klyqa
 # Author: Frederick Stallmeyer
 #
 # E-Mail: frederick.stallmeyer@gmx.de
 #
-#
 ###################################################################
+
+__version__ = "1.0.8"
+__author__ = 'Frederick Stallmeyer <fraizy@gmx.de>'
+__license__ = 'MIT'
 
 from .klyqa_ctl import Klyqa_account
 
