b555f38013f450a9d06349e564c900585bba5312
Frederick Stallmeyer <fraizy@gmx.de>
2022-10-20 10:13:06 +0200

    add main python file

---
+++ klyqa_ctl/__main__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Allow user to run main klyqa ctl script"""
+
+# Execute with:
+# $ python -m klyqa_ctl (3.9+)
+
+import klyqa_ctl.klyqa_ctl as klyqa_ctl
+
+if __name__ == '__main__':
+    klyqa_ctl.main()
