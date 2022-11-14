bfebe841cdd632e16ca3307e3dee2c5713744db0
Frederick Stallmeyer <fraizy@gmx.de>
2022-11-14 17:04:09 +0100

    add strenum dependency for pypi package, remove singleton-decorator

--- klyqa_ctl/__init__.py
+++ klyqa_ctl/__init__.py
@@ -11,7 +11,7 @@
 #
 ###################################################################
 
-__version__ = "1.0.10"
+__version__ = "1.0.11"
 __author__ = 'Frederick Stallmeyer <fraizy@gmx.de>'
 __license__ = 'MIT'
 
