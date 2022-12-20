cdf7ca905e806bde95f5a9e31a795165c3b9d33d
Frederick Stallmeyer <fraizy@gmx.de>
2022-12-20 12:33:31 +0100

    doxygen docs

--- klyqa_ctl/doc/html/search/functions_9.js
+++ klyqa_ctl/doc/html/search/functions_9.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['print_100',['print',['../classklyqa__ctl_1_1klyqa__ctl_1_1EventQueuePrinter.html#afe9617dcf747ed426530aa3207040221',1,'klyqa_ctl::klyqa_ctl::EventQueuePrinter']]],
   ['process_5fargs_5fto_5fmsg_5fcleaner_101',['process_args_to_msg_cleaner',['../namespaceklyqa__ctl_1_1devices_1_1vacuum.html#a38ef3ede603404a79585f44a78e3b6cc',1,'klyqa_ctl::devices::vacuum']]],
-  ['process_5fargs_5fto_5fmsg_5flighting_102',['process_args_to_msg_lighting',['../namespaceklyqa__ctl_1_1devices_1_1light.html#ad6f37dc3e920830783a253f17f896e06',1,'klyqa_ctl::devices::light']]]
+  ['process_5fargs_5fto_5fmsg_5flighting_102',['process_args_to_msg_lighting',['../namespaceklyqa__ctl_1_1devices_1_1light.html#a1fb122be5fed945b0730e3a42f9c5fd6',1,'klyqa_ctl::devices::light']]]
 ];
