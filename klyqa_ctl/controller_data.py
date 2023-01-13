5fc0da97f250dc3ea23618bf1be0d8db4d5cbace
Frederick Stallmeyer <fraizy@gmx.de>
2023-01-13 21:22:31 +0100

    Add property functions to controller data.

--- klyqa_ctl/controller_data.py
+++ klyqa_ctl/controller_data.py
@@ -3,9 +3,23 @@
 class ControllerData:
     """Controller data."""
 
-    def __init__(self, interactive_prompts: bool, offline: bool, ) -> None:
-        self.aes_keys: dict[str, bytes] = {}
-        self.interactive_prompts: bool = interactive_prompts
-        self.offline: bool = offline
-        
+    def __init__(self, interactive_prompts: bool = False, offline: bool = False) -> None:
+        self._attr_aes_keys: dict[str, bytes] = {}
+        self._attr_interactive_prompts: bool = interactive_prompts
+        self._attr_offline: bool = offline
+    
+    @property
+    def aes_keys(self) -> dict[str, bytes]:
+        """Return or set the devices dictionary."""
+        return self._attr_aes_keys
+    
+    @property
+    def interactive_prompts(self) -> bool:
+        """Return or set the devices dictionary."""
+        return self._attr_interactive_prompts
+    
+    @property
+    def offline(self) -> bool:
+        """Return or set the devices dictionary."""
+        return self._attr_offline
     
