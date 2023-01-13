38539cc8541f8da668839de8e6e59ca94ee80f6e
Frederick Stallmeyer <fraizy@gmx.de>
2023-01-13 21:39:51 +0100

    Local Connection add properties and setters.

--- klyqa_ctl/communication/local.py
+++ klyqa_ctl/communication/local.py
@@ -63,30 +63,106 @@ def send_msg(msg: str, device: KlyqaDevice, connection: LocalConnection) -> bool
 class LocalConnection:
     """LocalConnection"""
 
-    state: str = "WAIT_IV"
-    localIv: bytes = get_random_bytes(8)
-    remoteIv: bytes = b""
-
-    address: dict[str, str | int] = {"ip": "", "port": -1}
-    socket: socket.socket | None = None
-    received_packages: list[Any] = []
-    sent_msg_answer: dict[str, Any] = {}
-    aes_key_confirmed: bool = False
-    aes_key: bytes = b""
-
     def __init__(self) -> None:
-        self.state = "WAIT_IV"
-        self.localIv = get_random_bytes(8)
-
-        self.sendingAES: Any = None
-        self.receivingAES: Any = None
-        self.address = {"ip": "", "port": -1}
-        self.socket = None
-        self.received_packages = []
-        self.sent_msg_answer = {}
-        self.aes_key_confirmed = False
-        self.aes_key = b""
-        self.started: datetime.datetime = datetime.datetime.now()
+        self._attr_state: str = "WAIT_IV"
+        self._attr_localIv: bytes = get_random_bytes(8)
+        self._attr_sendingAES: Any = None
+        self._attr_receivingAES: Any = None
+        self._attr_address: dict[str, str | int] = {"ip": "", "port": -1}
+        self._attr_socket: socket.socket | None = None
+        self._attr_received_packages: list[Any] = []
+        self._attr_sent_msg_answer: dict[str, Any] = {}
+        self._attr_aes_key_confirmed: bool = False
+        self._attr_aes_key: bytes = b""
+        self._attr_started: datetime.datetime = datetime.datetime.now()
+        
+    @property
+    def state(self) -> str:
+        return self._attr_state
+    
+    @state.setter
+    def state(self, state: str) -> None:
+        self._attr_state = state
+    
+    @property
+    def localIv(self) -> bytes:
+        return self._attr_localIv
+    
+    @localIv.setter
+    def localIv(self, localIv: bytes) -> None:
+        self._attr_localIv = localIv
+    
+    @property
+    def sendingAES(self) -> Any:
+        return self._attr_sendingAES
+    
+    @sendingAES.setter
+    def sendingAES(self, sendingAES: Any) -> None:
+        self._attr_sendingAES = sendingAES
+    
+    @property
+    def receivingAES(self) -> Any:
+        return self._attr_receivingAES
+    
+    @receivingAES.setter
+    def receivingAES(self, receivingAES: Any) -> None:
+        self._attr_receivingAES = receivingAES
+    
+    @property
+    def address(self) -> dict[str, str | int]:
+        return self._attr_address
+    
+    @address.setter
+    def address(self, address: dict[str, str | int]) -> None:
+        self._attr_address = address
+    
+    @property
+    def socket(self) -> socket.socket | None:
+        return self._attr_socket
+    
+    @socket.setter
+    def socket(self, socket: socket.socket) -> None:
+        self._attr_socket = socket
+    
+    @property
+    def received_packages(self) -> list[Any]:
+        return self._attr_received_packages
+    
+    @received_packages.setter
+    def received_packages(self, received_packages: list[Any]) -> None:
+        self._attr_received_packages = received_packages
+    
+    @property
+    def sent_msg_answer(self) -> dict[str, Any]:
+        return self._attr_sent_msg_answer
+    
+    @sent_msg_answer.setter
+    def sent_msg_answer(self, sent_msg_answer: dict[str, Any) -> None:
+        self._attr_sent_msg_answer = sent_msg_answer
+    
+    @property
+    def aes_key_confirmed(self) -> bool:
+        return self._attr_aes_key_confirmed
+    
+    @aes_key_confirmed.setter
+    def aes_key_confirmed(self, aes_key_confirmed: bool) -> None:
+        self._attr_aes_key_confirmed = aes_key_confirmed
+    
+    @property
+    def aes_key(self) -> bytes:
+        return self._attr_aes_key
+    
+    @aes_key.setter
+    def aes_key(self, aes_key: bytes) -> None:
+        self._attr_aes_key = aes_key
+    
+    @property
+    def started(self) -> datetime.datetime:
+        return self._attr_started
+    
+    @started.setter
+    def started(self, started: datetime.datetime) -> None:
+        self._attr_started = started
         
 
 class LocalCommunication:
