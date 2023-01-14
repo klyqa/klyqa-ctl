48091fc5f4ee888ca17efb50a410ed93bda6844a
Frederick Stallmeyer <fraizy@gmx.de>
2023-01-14 10:22:51 +0100

    add account properties and setters

--- klyqa_ctl/account.py
+++ klyqa_ctl/account.py
@@ -36,21 +36,90 @@ class Account:
     ) -> None:
         """! Initialize the account with the login data, tcp, udp datacommunicator and tcp
         communication tasks."""
-        self.username: str = username
-        self.password: str = password
-        self.devices: dict[str, KlyqaDevice] = {}
-        self.settings: TypeJSON | None = {}
-        self.access_token: str = ""
-        self.username_cached: bool = False
-        self.settings_cached: bool = False
-        self._settings_loaded_ts: datetime.datetime | None = None
-        
-        self.device_configs: dict[Any, Any] = device_configs
-        
+        self._attr_username: str = username
+        self._attr_password: str = password
+        self._attr_devices: dict[str, KlyqaDevice] = {}
+        self._attr_settings: TypeJSON | None = {}
+        self._attr_access_token: str = ""
+        self._attr_username_cached: bool = False
+        self._attr_settings_cached: bool = False
+        self._attr__settings_loaded_ts: datetime.datetime | None = None
+        self._attr_device_configs: dict[Any, Any] = device_configs
         self._attr_settings_lock: asyncio.Lock = asyncio.Lock()
-
 
     @property
     def settings_lock(self) -> asyncio.Lock:
-        """Return the account settings lock."""
         return self._attr_settings_lock
+    
+    @property
+    def device_configs(self) -> dict[Any, Any]:
+        return self._attr_device_configs
+    
+    @device_configs.setter
+    def device_configs(self, device_configs: dict[Any, Any]) -> None:
+        self._attr_device_configs = device_configs
+    
+    @property
+    def username(self) -> str:
+        return self._attr_username
+    
+    @username.setter
+    def username(self, username: str) -> None:
+        self._attr_username = username
+    
+    @property
+    def password(self) -> str:
+        return self._attr_password
+    
+    @password.setter
+    def password(self, password: str) -> None:
+        self._attr_password = password
+    
+    @property
+    def devices(self) -> dict[str, KlyqaDevice]:
+        return self._attr_devices
+    
+    @devices.setter
+    def devices(self, devices: dict[str, KlyqaDevice]) -> None:
+        self._attr_devices = devices
+    
+    @property
+    def settings(self) -> TypeJSON | None:
+        return self._attr_settings
+    
+    @settings.setter
+    def settings(self, settings: TypeJSON | None) -> None:
+        self._attr_settings = settings
+    
+    @property
+    def access_token(self) -> str:
+        return self._attr_access_token
+    
+    @access_token.setter
+    def access_token(self, access_token: str) -> None:
+        self._attr_access_token = access_token
+    
+    @property
+    def username_cached(self) -> bool:
+        return self._attr_username_cached
+    
+    @username_cached.setter
+    def username_cached(self, username_cached: bool) -> None:
+        self._attr_username_cached = username_cached
+    
+    @property
+    def settings_cached(self) -> bool:
+        return self._attr_settings_cached
+    
+    @settings_cached.setter
+    def settings_cached(self, settings_cached: bool) -> None:
+        self._attr_settings_cached = settings_cached
+    
+    @property
+    def _settings_loaded_ts(self) -> datetime.datetime | None:
+        return self._attr__settings_loaded_ts
+    
+    @_settings_loaded_ts.setter
+    def _settings_loaded_ts(self, _settings_loaded_ts: datetime.datetime) -> None:
+        self._attr__settings_loaded_ts = _settings_loaded_ts
+    
