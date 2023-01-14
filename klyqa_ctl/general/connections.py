894482b884a64dcc50921d3553a18767b8327464
Frederick Stallmeyer <fraizy@gmx.de>
2023-01-14 11:50:55 +0100

    Cloud Connection add properties and setter functions.

--- klyqa_ctl/general/connections.py
+++ klyqa_ctl/general/connections.py
@@ -13,12 +13,28 @@ from klyqa_ctl.general.general import *
 class CloudConnection:
     """CloudConnection"""
 
-    received_packages: list[Any]
-    connected: bool
+    _attr_received_packages: list[Any]
+    _attr_connected: bool
 
     def __init__(self) -> None:
-        self.connected = False
-        self.received_packages = []
+        self._attr_connected = False
+        self._attr_received_packages = []
+        
+    @property
+    def received_packages(self) -> list[Any]:
+        return self._attr_received_packages
+
+    @received_packages.setter
+    def received_packages(self, received_packages: list[Any]) -> None:
+        self._attr_received_packages = received_packages
+
+    @property
+    def connected(self) -> bool:
+        return self._attr_connected
+
+    @connected.setter
+    def connected(self, connected: bool) -> None:
+        self._attr_connected = connected
 
 PROD_HOST: str = "https://app-api.prod.qconnex.io"
 TEST_HOST: str = "https://app-api.test.qconnex.io"
