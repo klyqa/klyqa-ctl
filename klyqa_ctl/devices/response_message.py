347b6ccf50d25d7eb65b04a74d9f8207bcac2f37
Frederick Stallmeyer <fraizy@gmx.de>
2023-01-15 04:26:37 +0100

    Add ResponseMessage getter and setter.

--- klyqa_ctl/devices/response_message.py
+++ klyqa_ctl/devices/response_message.py
@@ -8,8 +8,8 @@ class ResponseMessage:
     
     def __init__(self, **kwargs: Any) -> None:
         """__init__"""
-        self.type: str = ""
-        self.ts: datetime.datetime | None = None
+        self._attr_type: str = ""
+        self._attr_ts: datetime.datetime | None = None
         self.update(**kwargs)
 
     def update(self, **kwargs: Any) -> None:
@@ -19,3 +19,19 @@ class ResponseMessage:
         for attr in kwargs:
             if hasattr(self, attr):
                 setattr(self, attr, kwargs[attr])
+
+    @property
+    def type(self) -> str:
+        return self._attr_type
+
+    @type.setter
+    def type(self, type: str) -> None:
+        self._attr_type = type
+
+    @property
+    def ts(self) -> datetime.datetime | None:
+        return self._attr_ts
+
+    @ts.setter
+    def ts(self, ts: datetime.datetime | None) -> None:
+        self._attr_ts = ts
