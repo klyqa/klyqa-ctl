682fdb4aec46644099be2b2d075d05fe8ec0aaf5
Frederick Stallmeyer <fraizy@gmx.de>
2023-02-20 23:01:33 +0100

    Refactor message

--- klyqa_ctl/general/message.py
+++ klyqa_ctl/general/message.py
@@ -8,6 +8,7 @@ from enum import Enum
 from typing import Awaitable, Callable, Set
 
 from klyqa_ctl.general.general import (
+    DEFAULT_SEND_TIMEOUT_MS,
     LOGGER,
     Command,
     TypeJson,
@@ -31,11 +32,11 @@ MSG_COUNTER = 0
 class Message:
     """Message"""
 
-    started: datetime.datetime
-    _attr_target_uid: str
-    _attr_target_ip: str
     msg_queue: list[Command]
+    _attr_target_uid: str = ""
+    target_ip: str = ""
     msg_queue_sent: list[str] = field(default_factory=lambda: [])
+    started: datetime.datetime = datetime.datetime.now()
     state: MessageState = MessageState.UNSENT
     exception: Exception | None = None
     answered_datetime: datetime.datetime | None = None
@@ -46,7 +47,7 @@ class Message:
     # callback on error event or answer
     callback: Callable[[Message | None, str], Awaitable] | None = None
     cb_called: bool = False
-    time_to_live_secs: float = -1
+    time_to_live_secs: float = DEFAULT_SEND_TIMEOUT_MS
     msg_counter: int = -1
     send_try: int = 0
     aes_key: bytes | None = None
@@ -55,6 +56,7 @@ class Message:
         global MSG_COUNTER
         self.msg_counter = MSG_COUNTER
         MSG_COUNTER = MSG_COUNTER + 1
+        self.started = datetime.datetime.now()
 
     async def call_cb(self, uid_answered: str = "") -> None:
         if self.callback is not None and not self.cb_called:
@@ -88,14 +90,6 @@ class Message:
     @target_uid.setter
     def target_uid(self, target_uid: str) -> None:
         self._attr_target_uid = format_uid(target_uid)
-
-    @property
-    def target_ip(self) -> str:
-        return self._attr_target_ip
-
-    @target_ip.setter
-    def target_ip(self, target_ip: str) -> None:
-        self._attr_target_ip = target_ip
 
 
 @dataclass
